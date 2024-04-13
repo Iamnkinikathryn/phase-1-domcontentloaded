c31275907b93aaf5d75bc6c98906b6b706e0b6c5
Katherin Nkini <nkinikathryn@gmail.com>
2024-04-13 21:40:52 +0300

    e

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+// Your code goes
+document.addEventListener("DOMContentLoaded", function() {
+    var textElement = document.getElementById("text");
+    textElement.textContent = "This is really cool!";
+});
+
+
+
